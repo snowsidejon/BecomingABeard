93ca39977589ccdabd7cc64c2cd37e97a124506b
snowsidejon <jon@snowsidehosting.com>
2024-04-18 22:56:47 -0400

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
 		//Open street  Map
-		var coord = [40.738270, -74.008911]; // <--- coordinates here
+		var coord = [35.175016, -81.559679]; // <--- coordinates here
 
 		var map = L.map('map-canvas', { scrollWheelZoom:false}).setView(coord, 19);
 
